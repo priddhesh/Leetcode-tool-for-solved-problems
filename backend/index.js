d4137861cfddf6f62385edb114e4b630d109b7dd
priddhesh <109747774+priddhesh@users.noreply.github.com>
2024-03-04 23:18:18 +0530

    cors config updated

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: process.env.CLIENT_URL, 
+    origin: "*", 
     methods: ['GET', 'POST'], 
     allowedHeaders: ['Content-Type', 'Authorization'] 
   };
